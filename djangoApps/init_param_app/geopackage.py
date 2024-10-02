59d062d9ad81986163e1d5d666d7b20d56be32fa
joe work <Lavern.J.Work@rtx.com>
2024-10-02 22:02:28 +0000

    NGWPC-3938 - Added code to prevent geopackage rebuilds

--- djangoApps/init_param_app/geopackage.py
+++ djangoApps/init_param_app/geopackage.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 import inspect
 #import subprocess
 from subprocess import run
@@ -33,6 +34,7 @@ def get_geopackage(gage_id, source, domain, keep_file=False):
     #temp dir for files
     loc_temp_dir = None
     try:
+        
         loc_temp_dir = gage_file_mgmt.get_local_temp_directory(data_type, gage_id)
 
         # hydrofabric input data directory
@@ -43,13 +45,18 @@ def get_geopackage(gage_id, source, domain, keep_file=False):
         hydrofabric_type = config['hydrofabric_type']
         # Tell the subsetter what to retrieve
         subsetter_gage_id = f"Gages-{gage_id}"
-
+        
         gpkg_filename = gage_file_mgmt.get_geopackage_filename(gage_id)
-
         status_str = "Calling HF Subsetter R code"
-        logger.debug(status_str)
-
+        
         #Call R code for subsetter
+        logger.debug(status_str)
+        
+        path_string = "R/run_subsetter.R"
+        grandparent_dir = os.path.dirname(settings.BASE_DIR)
+        directory = os.path.join(grandparent_dir, path_string)
+        logger.debug(f"R code file = {directory}")
+
         run_command = ["/usr/bin/Rscript", "../R/run_subsetter.R",
                        subsetter_gage_id,
                        loc_temp_dir,
@@ -57,9 +64,8 @@ def get_geopackage(gage_id, source, domain, keep_file=False):
                        hydrofabric_dir,
                        hydrofabric_version.lstrip('v'),
                        hydrofabric_type]
-
+        
         result = run(run_command, capture_output=True)
-
         if 'error' in str(result.stderr):
             error_str = 'Hydrofabric subsetting failed; check gage id.'
             error = {'error': error_str}
@@ -74,7 +80,7 @@ def get_geopackage(gage_id, source, domain, keep_file=False):
 
     # Write geopackage to s3 bucket
     try:
-        result, insert_time, uri = gage_file_mgmt.write_file_to_s3(gage_id, domain, data_type, source, loc_temp_dir,
+        uri = gage_file_mgmt.write_file_to_s3(gage_id, domain, data_type, source, loc_temp_dir,
                                                                    gpkg_filename)
     # TODO PROPERLY HANDEL LOGGING "RESPONSE" FOR CAUGHT ERRORS
     except psycopg2.DatabaseError as psycopg2_error:
@@ -87,14 +93,7 @@ def get_geopackage(gage_id, source, domain, keep_file=False):
     # Clean up temp files
     if not keep_file:
         # remove temp file
-        local_file = loc_temp_dir + gpkg_filename
-        try:
-            os.remove(local_file)
-            logging.debug(f"Deleted: {local_file}")
-        except Exception as e:
-            print(f"Error deleting {local_file}: {e}")
-            logging.warning(f"Error deleting {local_file}: {e}")
+        gage_file_mgmt.delete_local_temp_directory(loc_temp_dir)
 
     # TODO PROPERLY HANDEL LOGGING "RESPONSE" FOR CAUGHT ERRORS
-    geopackage_output = dict(uri=uri)
-    return geopackage_output
+    return uri
