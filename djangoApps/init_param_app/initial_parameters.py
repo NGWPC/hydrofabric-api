246a8357d74e3097c8a30d4d96332fea9e062341
Brock Hinkson <brock.w.hinkson@rtx.com>
2024-11-19 23:17:32 +0000

    NGWPC-3990 - Create LASAM Module

--- djangoApps/init_param_app/initial_parameters.py
+++ djangoApps/init_param_app/initial_parameters.py
@@ -10,6 +10,7 @@ from .t_route import *
 from .sac_sma import *
 from .snow17 import *
 from .sft import *
+from djangoApps.init_param_app import lasam_ipe
 
 # Setup logging
 logger = logging.getLogger(__name__)
@@ -118,6 +119,8 @@ def calculate_module_params(gage_id, source, domain, module, subset_dir, gpkg_fi
         results = snow17_ipe(gage_id, source, domain, subset_dir, gpkg_file, module_metadata, gage_file_mgmt)
     elif module == "Sac-SMA":
         results = sac_sma_ipe(gage_id, source, domain, subset_dir, gpkg_file, module_metadata, gage_file_mgmt)
+    elif module == "LASAM":
+        results = lasam_ipe(gage_id, source, domain, subset_dir, gpkg_file, module_metadata, gage_file_mgmt)
     else:
         error_str = "Module name not valid:" + module
         error = dict(error=error_str)
