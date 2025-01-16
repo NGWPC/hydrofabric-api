69f76450e50433e5ca44232159289450d18c45b3
Daniel Cumpton <dccumpton@rtx.com>
2025-01-16 03:20:44 +0000

    removing R code from CFE

--- djangoApps/init_param_app/cfe.py
+++ djangoApps/init_param_app/cfe.py
@@ -1,10 +1,13 @@
 import subprocess
 import json
+import pandas as pd
 
 from .models import CfeParams
 from .util import utilities
 from .util.enums import FileTypeEnum
 from .util.utilities import *
+from .hf_attributes import *
+
 
 # Setup logging
 logger = logging.getLogger(__name__)
@@ -24,98 +27,73 @@ def cfe_ipe(module, version, gage_id, source, domain, subset_dir, gpkg_file, mod
     dict: JSON output with cfg file URI, calibratable parameters initial values, output variables.
     '''
     cfe_x_params = ('a_Xinanjiang_inflection_point_parameter', 'b_Xinanjiang_shape_parameter', 'x_Xinanjiang_shape_parameter', 'urban_decimal_fraction')
-    cfe_s = 'CFE-S'
+
+    if module == 'CFE-S':
+        scheme = 'Schaake'
+    elif module == 'CFE-X':
+        scheme = 'Xinanjiang'
+
+    attr_name_key = f"{version}_name"
+
     # Get config file for paths
     config = get_config()
-    input_dir = config['input_dir'] 
-    sys_root = os.path.dirname(settings.BASE_DIR)
-    app_path = os.path.join(settings.BASE_DIR, 'init_param_app')
-    cfe_x_json = 'CFE-X.json'
-    cfe_s_json = 'CFE-S.json'
-    r_code_file = f"{sys_root}/R/run_create_cfe_init_bmi_config.R"
+    input_dir = config['input_dir']
 
-    #Build input arguments for config file R script
-    #create string containing R c (combine) function and gage IDs
-    gage_id_string = "'"+gage_id+"'"
+    filename_list = []
+        
+    from_attr = CfeParams.objects.filter(source_file='attr').values('name', 'nwm_name', 'default_value')
+    consts = CfeParams.objects.filter(source_file='const').values('name', 'nwm_name', 'default_value')
 
-    # TODO: This will be replaced with a call to the database when connectivity is available in the container
-    #cfe_json = os.path.join(app_path, cfe_x_json) if module == 'CFE-X' else os.path.join(app_path, cfe_s_json)
- 
-    #try:
-    #    import_json = open(cfe_json)
-    #    parameters = json.load(import_json)
-    #except Exception as e:
-    #    logger.error(f"Error reading JSON document {cfe_json} Error is: {e}")
-    #    error_str = f"CFE error reading JSON document {cfe_json}"
-    #    error = dict(error = error_str)
-    #    return error
+    divide_attr = get_hydrofabric_attributes(gpkg_file, version)
 
-    # get the cfe parameter from the DB
-    queryset = CfeParams.objects.values('name', 'nwm_name', 'default_value')
+    catchments = divide_attr["divide_id"].tolist()
 
-    # Create lists for passing CFE parameter names and constant values to R code
-    cfe_parameters_nwm_name = []
-    cfe_parameters_cfe_name =  []
-    cfe_parameters_const_name = []
-    cfe_parameters_const_value = []
+    parameters_df = pd.read_csv(f'{input_dir}/cfe_x.csv')
+    filtered_parameters = parameters_df[parameters_df['divide_id'].isin(catchments)]
 
-    for row in queryset:
-        # We do not need to check for CFE-S or CFE-X on this check because the four
-        # parmeter in cfe_x_params do not have default values
-        if row['default_value'] is not None:
-            cfe_parameters_const_name.append("'" + row['name'] + "'") 
-            cfe_parameters_const_value.append("'"  + row['default_value'] + "'")
-            
-        # We need to check for CFE-S or CFE-X on this check because the three of the four
-        # parmeter in cfe_x_params have an nwm name
-        if row['nwm_name'] is not None:
-            if module == cfe_s and row['name'] in cfe_x_params:
-                # skip this row it is a CFE-X parameter
-                continue
-            else:
-                cfe_parameters_nwm_name.append("'" + row["nwm_name"] + "'")
-                cfe_parameters_cfe_name.append("'"  + row['name'] + "'")
+    #Join parameters from csv, area, and attribute file into single dataframe using divide_id as index
+    df_all = filtered_parameters.join(divide_attr.set_index('divide_id'), on='divide_id')
 
-    cfe_parameters_const_name = '"c(' + ",".join(cfe_parameters_const_name) + ')"' 
-    cfe_parameters_const_value = '"c(' + ",".join(cfe_parameters_const_value) + ')"' 
-    cfe_parameters_nwm_name = '"c(' + ",".join(cfe_parameters_nwm_name) + ')"' 
-    cfe_parameters_cfe_name = '"c(' + ",".join(cfe_parameters_cfe_name) + ')"' 
+    if(module == 'CFE-S'):
+        for param in cfe_x_params:
+           df_all.drop(param, axis=1, inplace=True)
+           
+    #print(df_all)
 
-    #TODO make the Rscript string below a constant
-    run_command = [f"/usr/bin/Rscript {r_code_file}", 
-    gage_id_string,
-    input_dir,
-    subset_dir,
-    cfe_parameters_const_name,
-    cfe_parameters_const_value,
-    cfe_parameters_nwm_name,
-    cfe_parameters_cfe_name,
-    module,
-    sys_root,
-    gpkg_file]
+    for index, divide in df_all.iterrows():
 
-    run_command_string = " ".join(run_command)
+        divide_id = divide['divide_id']
+        cfg_filename = f'{divide_id}_bmi_config_cfe.txt'
+        filename_list.append(cfg_filename)
+        cfg_filename_path = os.path.join(subset_dir, cfg_filename)
+    
+        with open(cfg_filename_path, 'w') as outfile:
+            #write non-parameter items            
+            outfile.write('forcing_file=BMI\n')
+            outfile.write('verbosity=0\n')
+            outfile.write(f'surface_partitioning_scheme={scheme}\n')
+            outfile.write('num_timesteps=0\n')
 
-    status_str = "Running CFE IPE R code"
-    logger.info(status_str)
+            #write items from divide attributes and CFE-X csv file
+            for param in from_attr:
+                param_name = param['name']
+                attr_name = json.loads(param['nwm_name'])[attr_name_key]
+                attr_value = divide[attr_name]
+                cfg_line = f"{param_name}={attr_value}\n"
+                outfile.write(cfg_line)
 
-    try:
-        subprocess.call(run_command_string, shell=True) 
-    except:
-        error_str = "CFE IPE R code failure"
-        error = dict(error = error_str) 
-        logger.error(error_str)
-        return error
-    # Since the R code writes the files to the subset_dir get list of files to send S3
-    filename_list = utilities.get_subset_dir_file_names(subset_dir)
-
-    # TODO check if filename_list is empty, and throw error? other??
-
+            #write constants
+            for param in consts:
+                param_name = param['name']
+                attr_value = param['default_value']
+                cfg_line = f"{param_name}={attr_value}\n"
+                outfile.write(cfg_line)
+    
     # Write files to DB and S3
     uri = gage_file_mgmt.write_file_to_s3(gage_id, version, domain, FileTypeEnum.PARAMS, source, subset_dir, filename_list, module=module)
     status_str = "Config files written to:  " + uri
     logger.info(status_str)
-
+    
     #write s3 location and ipe values from one of the subset_dir files to output json
     file = os.path.join(subset_dir, filename_list[0])
     with open(file, 'r') as file:
